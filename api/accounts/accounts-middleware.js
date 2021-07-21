e67e1622f2e293d79acf60d993d8d1732433bd31
Sarah Guidry <sarahbethguidry@gmail.com>
2021-07-21 12:10:47 -0500

    checkAccountPayload middleware done

--- api/accounts/accounts-middleware.js
+++ api/accounts/accounts-middleware.js
@@ -1,5 +1,38 @@
-exports.checkAccountPayload = (req, res, next) => {
+const Accounts = require('./accounts-model')
+
+exports.checkAccountPayload = async (req, res, next) => {
   // DO YOUR MAGIC
+  try {
+    const changes = req.body
+    if (!changes.name || !changes.budget) {
+      next({
+        status: 400,
+        message: 'name and budget are required'
+      })
+    } else if (typeof changes.name !== 'string') {
+      next({
+        status: 400,
+        message: 'name of account must be a string'
+      })
+    } else if (changes.name.trim().length < 3 || changes.name.trim().length > 100) {
+      next({
+        status: 400,
+        message: 'name of account must be between 3 and 100'
+      })
+    } else if (changes.budget.isNaN()) {
+      next({
+        status: 400,
+        message: 'budget of account must be a number',
+      })
+    } else if (changes.budget < 0 || changes.budget > 1000000) {
+      next({
+        status: 400,
+        message: 'budget of account is too large or too small'
+      })
+    }
+  } catch (err) {
+    next(err)
+  }
 }
 
 exports.checkAccountNameUnique = (req, res, next) => {
